2b28c22f86d763982d4236ccb5c18daba70c9b72
Aaron <clonemeagain@gmail.com>
2017-10-12 22:56:37 +1100

    Limited progress.. looks prettier, added more screenshots of assumed config

--- replaceStateAndIndicateSuccess.js
+++ replaceStateAndIndicateSuccess.js
@@ -18,50 +18,65 @@
                         token: data.token
                     };
                 };
+                console.log(data);
                 $('#ticketInfo').append('<div id="dialog" style="display:none;">' +
                         '<div class="feedback-heading" style="padding:10px;">' + data.dialog_heading + '</div>' +
-                        '<div class="feedback-input" style="margin:10px;"><textarea name="text" value="" placeholder="' + data.suggestion + '"></textarea></div>' +
+                        '<div class="votegroup green-border"><input type="radio" name="vote" value="up" id="upvote"/>' +
+                        '<label for="upvote">' + data.options.up + '</label></div>' +
+                        '<div class="votegroup"><input type="radio" name="vote" value="meh" id="mehvote"/>' +
+                        '<label for="mehvote">' + data.options.meh + '</label></div>' +
+                        '<div class="votegroup red-border"><input type="radio" name="vote" value="down" id="downvote"/>' +
+                        '<label for="downvote">' + data.options.down + '</label></div>' +
+                        '<div class="feedback-input"><textarea name="text" value="" placeholder="' + data.options.placeholder + '"></textarea></div>' +
                         '</div>');
                 // The page has loaded, we're showing the modal, let's save the feedback now, and if they update the input and send, we'll update it.
                 $.ajax({type: 'post', url: data.url, data: getData()});
+
+                // Select the selected one, but listen if it changes
+                $('.votegroup').on('click', function (e) {
+                    var elem = $(e.target);
+                    data.vote = elem.val();
+                    $('.votegroup span.check').remove();
+                    elem.parent().prepend('<span class="check">&#x2714;</span>');
+                });
+                $('#' + data.vote + 'vote').click();
                 // Show the modal dialog to the user asking for additional comments:
-                var dialog = $('#dialog')
-                        .dialog({
-                            modal: true,
-                            height: 410,
-                            minWidth: 592,
-                            show: "blind",
-                            hide: "blind",
-                            closeText: '', //data.close_button_text,
-                            position: {my: "center", at: "center", of: '#ticketInfo'},
-                            buttons: [{
-                                    text: data.send_button_text,
-                                    icon: "ui-icon-heart",
-                                    "class": "feedback-sendButton",
-                                    click: function () {
-                                        $.ajax({
-                                            type: 'post',
-                                            url: data.url,
-                                            data: getData(),
-                                            success: function (msg) {
-                                                $('#ticketInfo').after('<div style="background-color: lightgreen; border: 1px solid green; padding:10px;">' + data.good + '</div>');
-                                            },
-                                            error: function (xhr) {
-                                                $('#ticketInfo').after('<div style="background-color: pink; border: 1px solid red; padding:10px;">' + data.bad + '</div>');
-                                                console.log(xhr);
-                                            },
-                                            complete: function () {
-                                                $(".ui-dialog-titlebar-close").click();
-                                            }
-                                        });
-                                    }}],
-                            close: function (event, ui) {
-                                $(this).dialog('destroy');
-                            }
-                        });
+                $('#dialog').dialog({
+                    modal: true,
+                    height: 410,
+                    minWidth: 592,
+                    show: "blind",
+                    hide: "blind",
+                    closeText: '', //data.close_button_text,
+                    position: {my: "center", at: "center", of: '#ticketInfo'},
+                    buttons: [{
+                            text: data.send_button_text,
+                            icon: "ui-icon-heart",
+                            "class": "feedback-sendButton",
+                            click: function () {
+                                $.ajax({
+                                    type: 'post',
+                                    url: data.url,
+                                    data: getData(),
+                                    success: function (msg) {
+                                        $('#ticketInfo').after('<div style="background-color: lightgreen; border: 1px solid green; padding:10px;">' + data.good + '</div>');
+                                    },
+                                    error: function (xhr) {
+                                        $('#ticketInfo').after('<div style="background-color: pink; border: 1px solid red; padding:10px;">' + data.bad + '</div>');
+                                        console.log(xhr);
+                                    },
+                                    complete: function () {
+                                        $(".ui-dialog-titlebar-close").click();
+                                    }
+                                });
+                            }}],
+                    close: function (event, ui) {
+                        $(this).dialog('destroy');
+                    }
+                });
                 console.log("Plugin: Feedback has run.");
                 // for debugging, just return here, otherwise you have to keep adding the key
-                //return true;
+                return true;
                 // Clear the url part from the back-button.. 
                 // ergo: View Source won't show this
                 if (window.history.replaceState) {
@@ -69,9 +84,6 @@
                     // not 100% that this actually works.. 
                     window.history.replaceState({}, document.title, newhref); // we were never here..
                     window.history.pushState({}, document.title, newhref);
-                } else {
-                    // Cry?.. fucking IE..
-                    //document.location.href = newhref;
                 }
             });
 })(jQuery);
